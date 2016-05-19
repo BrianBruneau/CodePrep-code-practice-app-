df3a19ea28dc18e82e81fbbb86267f17f2e3bca0
BrianBruneau <Brian8ax@gmail.com>
2016-05-19 11:13:10 -0700

    added some countdown logic

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,51 @@
  $( document ).ready(function(){
    $(".button-collapse").sideNav();
  });
+
+function setWebState(webstate) {
+    CURRENT_STATE = webstate;
+    executeState();
+}
+
+function executeState() {
+  if (CURRENT_STATE === NO_STATE) {
+    countdown( "countdown", 1, 30 );
+
+  } else if (CURRENT_STATE === EASY_STATE) {
+
+  } else if (CURRENT_STATE === MEDIUM_STATE) {
+
+  } else if (CURRENT_STATE === HARD_STATE)
+
+}
+
+
+
+ function countdown( elementName, minutes, seconds )
+{
+    var element, endTime, hours, mins, msLeft, time;
+
+    function twoDigits( n )
+    {
+        return (n <= 9 ? "0" + n : n);
+    }
+
+    function updateTimer()
+    {
+        msLeft = endTime - (+new Date);
+        if ( msLeft < 1000 ) {
+            element.innerHTML = "countdown's over!";
+        } else {
+            time = new Date( msLeft );
+            hours = time.getUTCHours();
+            mins = time.getUTCMinutes();
+            element.innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );
+            setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
+        }
+    }
+
+    element = document.getElementById( elementName );
+    endTime = (+new Date) + 1000 * (60*minutes + seconds) + 500;
+    updateTimer();
+}
+
